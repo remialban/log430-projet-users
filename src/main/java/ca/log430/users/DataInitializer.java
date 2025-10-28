ca87f197ad093964aa6458dc2f2f5b4ee69e25b0
Rémi Alban <remi.alban.1@ens.etsmtl.ca>
2025-10-28 17:42:20 -0400

    add data initializer to populate initial user data

--- src/main/java/ca/log430/users/DataInitializer.java
+++ src/main/java/ca/log430/users/DataInitializer.java
@@ -5,6 +5,8 @@ import ca.log430.users.ports.out.UserRepositoryOut;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDate;
+
 @Component
 public class DataInitializer implements CommandLineRunner {
     private final UserRepositoryOut userRepository;
@@ -16,8 +18,8 @@ public class DataInitializer implements CommandLineRunner {
     @Override
     public void run(String... args) {
         if (userRepository.count() == 0) {
-            userRepository.save(new User("Alice", "alice@example.com"));
-            userRepository.save(new User("Bob", "bob@example.com"));
+            userRepository.save(new User("alice@example.com", "passwordAlice", "Alice", "Street 1", LocalDate.of(2000, 1, 1)));
+            userRepository.save(new User("bob@example.com", "passwordBob", "Boob", "Street 2", LocalDate.of(2000, 2, 1)));
             System.out.println("✅ Données initiales insérées !");
         }
     }
